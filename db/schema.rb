316c8dd471f793dbb62d869735389af1ff123b26
Gabriel Pessanha <gabrielcmp1@gmail.com>
2017-11-28 18:19:53 -0200

    fixes for missing photo, navbar, index and show visibility

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20171128134449) do
+ActiveRecord::Schema.define(version: 20171128191442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
