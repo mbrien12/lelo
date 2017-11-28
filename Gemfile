2920437565953101fb1a21d85c793de3135c0bd1
Michelle Brien <michelle.brien12@gmail.com>
2017-11-28 11:24:35 -0200

    created seeds

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'jquery-rails'
 gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
+gem 'faker'
 
 gem "geocoder"
 gem "coffee-rails"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,52 @@
+require 'faker'
+
 puts 'Cleaning database...'
-User.destroy_all
 Skill.destroy_all
+User.destroy_all
+
+
+
+puts "Creating 2 teachers with 1 skill each"
+
+  teacher1 = User.create(
+    email: "teacher1@teacher1.com",
+    password: Faker::Internet.password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    location: "Nex, Gloria, Rio",
+    latitude: 22.92084,
+    longitude: -43.17809,
+    photo: "https://assets.entrepreneur.com/content/3x2/1300/20150406145944-dos-donts-taking-perfect-linkedin-profile-picture-selfie-mobile-camera-2.jpeg",
+    role: "teacher",
+    bio: "Hello I'm a teacher"
+    )
+
+  teacher1.skills.create(
+    name: "Ruby",
+    description: "I can teach you how to create a Ruby on Rails application",
+    price: "$10/hr",
+    category: "Programming"
+    )
+
+  teacher2 = User.create(
+    email: "teacher2@teacher2.com",
+    password: Faker::Internet.password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    location: "Copacabana beach, Rio",
+    latitude: 22.96944,
+    longitude: 43.1822416,
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJFRuAejYIvKte1YL7eTyYc-szQKEDtOTtQlPOPWs6QtkHy3T_eg",
+    role: "teacher",
+    bio: "Hello I'm another teacher"
+    )
+
+   teacher2.skills.create(
+    name: "Guitar lessons",
+    description: "I'm a professional guitar player, come jam with me to improve your skills!",
+    price: "$8/hr",
+    category: "Music"
+    )
+
+
+
