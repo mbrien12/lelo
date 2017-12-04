5906636b5c975413cd3746b9f6a5348f1d64df6c
Michelle Brien <michelle.brien12@gmail.com>
2017-12-04 14:33:58 -0200

    added api method to controller and showing name and date in view as list

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
@@ -9,5 +12,30 @@ class ProfilesController < ApplicationController
     else
       @lessons = @user.lessons
     end
+
+    @lessons.each do |lesson|
+      @lesson = lesson
+    end
+
+    # Meetup API call to get related topics and location for learners
+    @key = "12422e59366e34213a1d2c1836652d19"
+
+    @response = open("https://api.meetup.com/2/open_events?&sign=true&photo-host=public&lat=#{@lesson.skill.latitude}&text=#{@lesson.skill.name}&lon=#{@lesson.skill.longitude}&radius=5&page=20&key=#{@key}")
+    @json = JSON.parse(@response.read)
+
+
+
+
+
+
   end
 end
+
+
+# latitude = lesson.skill.latitude
+# longitude = lesson.skill.longitude
+#  topic = lesson.skill.name
+#  key = "12422e59366e34213a1d2c1836652d19"
+#  response = open("https://api.meetup.com/2/open_events?&sign=true&photo-host=public&lat=#{latitude}&text=#{topic}&lon=#{longitude}&radius=5&page=20&key=#{key}")
+#  json = JSON.parse(response.read)
+#  return json
