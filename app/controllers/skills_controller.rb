90021ab0ae2d881d1cc18b622b0c1eb8bf3296d0
Amy Hogue <hogue.a@gmail.com>
2017-12-05 14:44:40 -0200

    added somelinks to profile page

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -44,7 +44,7 @@ class SkillsController < ApplicationController
 
     respond_to do |format|
       if @skill.save
-        format.html { redirect_to @skill, notice: 'Skill was successfully created.' }
+        format.html { redirect_to profile_path(current_user), notice: 'Skill was successfully created.' }
         format.json { render :show, status: :created, location: @skill }
       else
         format.html { render :new }
