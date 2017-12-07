d0790eb452ce97a454690b08ed070c507bfffa77
Gabriel Pessanha <gabrielcmp1@gmail.com>
2017-12-07 18:57:58 -0200

    working with show more, but it need styling

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,19 +4,19 @@ require 'json'
 class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @skills = @user.skills # Skill.where(teacher: @user)
+    @skills = @user.skills.order(created_at: :desc) # Skill.where(teacher: @user)
     @review = Review.new
     @message = Message.new
 
     if @user.role == 'teacher'
       @lessons = @skills.flat_map { |skill| skill.lessons }
     else
-      @lessons = @user.lessons
+      @lessons = @user.lessons.order(created_at: :desc)
     end
 
 
     skills = []
-    lessons = @user.lessons
+    lessons = @user.lessons.order(created_at: :desc)
     lessons.each { |lesson| skills << lesson.skill }
     @unique_skills = skills.uniq
 
@@ -30,14 +30,16 @@ class ProfilesController < ApplicationController
 
     # Meetup API call to get related topics and location for learners
 
+    @responses = []
     @lessons.each do |lesson|
       @lesson = lesson
-      @responses = []
       if !@lesson.nil?
         @response = open(
-        "https://api.meetup.com/find/groups?&sign=true&photo-host=public&lon=#{@lesson.skill.longitude}&text=#{@lesson.skill.category}&radius=5&lat=#{@lesson.skill.latitude}&page=6&key=#{ENV['MEETUP_KEY']}")
+        "https://api.meetup.com/find/groups?&sign=true&photo-host=public&lon=#{@lesson.skill.longitude}&text=#{@lesson.skill.category}&radius=5&lat=#{@lesson.skill.latitude}&page=15&key=#{ENV['MEETUP_KEY']}")
         @json = JSON.parse(@response.read)
+        puts @json[0]['score'] unless @json[0].nil?
         @responses << @json
+        p @responses.size
       end
     end
 
